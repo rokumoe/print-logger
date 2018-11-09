57ce04335bdee14ff485f9c770356a1426feb7b3
vizee <vizee@live.cn>
2018-11-10 02:39:55 +0800

    bit

---
+++ README.md
@@ -0,0 +1,8 @@
+# print-logger
+
+## usage
+
+```
+[dependencies]
+print-logger = { git = "https://github.com/rokumoe/print-logger.git" }
+```
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+extern crate log;
+
+use std::time::{Duration, SystemTime};
+
+struct Logger;
+
+impl log::Log for Logger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        let elapsed = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap() - *EPOCH;
+        println!("[{:04}.{:03}] {} {}", elapsed.as_secs(), elapsed.subsec_millis(), record.level(), record.args());
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: Logger = Logger;
+static mut _EPOCH: Duration = Duration::from_secs(0);
+static EPOCH: &Duration = unsafe { &_EPOCH };
+
+pub fn set_level(lv: log::LevelFilter) {
+    log::set_max_level(lv);
+}
+
+pub fn init() -> Result<(), log::SetLoggerError> {
+    unsafe {
+        _EPOCH = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    }
+    log::set_logger(&LOGGER).map(|()| set_level(log::LevelFilter::Info))
+}
